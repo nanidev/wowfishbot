1ef3c515accc38e42e8560c26d594226c5a98fcd
MentalNoob <kingalex966@gmail.com>
2026-09-10 11:56:18 +0000

    Delete Models directory

--- Models/BobberPixelSample.cs
+++
@@ -1,8 +0,0 @@
-namespace wowfishbot.Models;
-
-public sealed record BobberPixelSample(
-    int OffsetX,
-    int OffsetY,
-    int Red,
-    int Green,
-    int Blue);
--- Models/BobberSearchArea.cs
+++
@@ -1,6 +0,0 @@
-namespace wowfishbot.Models;
-
-public sealed record BobberSearchArea(int X, int Y, int Width, int Height)
-{
-    public bool IsValid => Width > 0 && Height > 0;
-}
